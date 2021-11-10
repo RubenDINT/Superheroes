39ceccb987890cfb79fcb40b08f9699d3cfc13fd
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2021-11-10 10:41:50 +0100

    l

--- Superheroes/MainWindow.xaml.cs
+++ Superheroes/MainWindow.xaml.cs
@@ -20,13 +20,37 @@ namespace Superheroes
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Superheroe> listaSuperheroes = Superheroe.GetSamples();
+        int posicion = 0;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            List<Superheroe> listaSuperheroes = Superheroe.GetSamples();
-            PrincipalDockPanel.DataContext = listaSuperheroes[0];
+            PrincipalDockPanel.DataContext = listaSuperheroes[posicion];
 
+        }
+
+        private void FlechaIzquierda_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (posicion > 0)
+            {
+                posicion--;
+                PrincipalDockPanel.DataContext = listaSuperheroes[posicion];
+                NumeroPagina.Text = posicion + 1 + " /3";
+            }
+
+        }
+
+        private void FlechaDerecha_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (posicion < listaSuperheroes.Count - 1)
+            {
+                posicion++;
+                PrincipalDockPanel.DataContext = listaSuperheroes[posicion];
+                NumeroPagina.Text = posicion + 1 + " /3";
+            }
         }
     }
 }
